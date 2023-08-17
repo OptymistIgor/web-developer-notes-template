35308d9240801b57f37361c1e24913f66b477837
Ihor Optymist <ihor.optymist@gmail.com>
2023-08-17 19:46:59 +0300

    Git coomit revert

---
+++ My notes/DelveNetwork/UI/git.tsx
@@ -0,0 +1,5 @@
+// https://www.git-tower.com/learn/git/commands/git-revert
+
+// git revert 29b613521fc2c14c16a88c0ae95791f82b390422 --no-commit
+
+// як повернути попередні зиіни, якщо зробив зміни
