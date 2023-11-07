6cd3d20d7cd748b57691e86ba268e8b1da26ecc4
Ihor Optymist <ihor.optymist@gmail.com>
2023-11-07 19:35:41 +0100

    4.2 for

---
+++ My notes/Okten/learn JS/4.2 for.js
@@ -0,0 +1,11 @@
+let users = [
+    {skills: ['html', 'js', 'Angular '], id: 1, name: 'Ivan', age:50, status: true},
+    {skills: ['html', 'mysql', 'React'], id: 2, name: 'Bogdana', age:14, status: true},
+    {skills: ['html', 'js', 'Angular'], id: 3, name: 'Igor', age:42, status: true},
+    {skills: ['html', 'mongo', 'React'], id: 4, name: 'Ira', age:30, status: true}, 
+ ];
+debugger;
+ for (let i = 0; i < users.length; i++){
+     console.log('test')
+     console.log(users[i])
+ }
