ef6fd75f2603726809c9e8c73b66377880d56d65
Ihor Optymist <ihor.optymist@gmail.com>
2023-11-08 13:22:16 +0100

    4.3 for off

---
+++ My notes/Okten/learn JS/4.3 for off.js
@@ -0,0 +1,11 @@
+let users = [
+    {skills: ['html', 'js', 'Angular '], id: 1, name: 'Ivan', age:50, status: true},
+    {skills: ['html', 'mysql', 'React'], id: 2, name: 'Bogdana', age:14, status: true},
+    {skills: ['html', 'js', 'Angular'], id: 3, name: 'Igor', age:42, status: true},
+    {skills: ['html', 'mongo', 'React'], id: 4, name: 'Ira', age:30, status: true}, 
+ ];
+
+ for (let user of users ){
+    console.log(user)
+ }
+ 
