f42777171af5223f3c65bc227bae1a2460a78f1c
Ihor Optymist <ihor.optymist@gmail.com>
2023-11-10 19:24:20 +0100

    6.1 func_str_1

---
+++ My notes/Okten/learn JS/6.1 func_string_1.js
@@ -0,0 +1,13 @@
+let str = 'Hello Okten';
+console.log(str.toLocaleLowerCase());
+console.log(str.toLocaleUpperCase());
+console.log(str.indexOf());
+console.log(str.charAt(8));
+console.log(str.replaceAll('e', '!'));
+// console.log(str.replace('e', '!'));
+let split = str.split(' ');
+console.log(split);
+
+
+
+
